45da3365a4220ea298956e5730a2da88e071affa
VictorPozsgay <victor.pozsgay@gmail.com>
2026-03-12 20:38:17 +0000

    formatted paleo, forcing, sims, GST logger

---
+++ src/KlondikeSlides/constants.py
@@ -0,0 +1,7 @@
+# This Python file uses the following encoding: utf-8
+# This module defines the constants needed for the package
+
+colorcycle = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+              '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+
+holocene_end_kaBP = 11.7
--- src/KlondikeSlides/format_raw_data.py
+++ src/KlondikeSlides/format_raw_data.py
@@ -15,6 +15,8 @@ import os
 import pickle
 from functools import reduce
 
+from KlondikeSlides.constants import holocene_end_kaBP
+
 
 def format_wildfire_data(path_in, path_out):
     """ Function formats wildfire data into a csv
@@ -114,9 +116,161 @@ def format_forcing_ygs(path_in, path_out):
     with open(os.path.join(path_out, 'df_airT.pkl'), 'wb') as file: 
         # A new file will be created 
         pickle.dump(df_airT, file)
-        
 
-def format_all_raw_data(path_wildfire_in, path_wildfire_out, path_gst_logger_in, path_gst_logger_out, path_forcing_ygs_sites_in, path_forcing_ygs_sites_out):
+def format_simulations_ygs(path_database, path_in, path_out):
+    """ Function formats simulation data into a netCDF pickle
+    
+    Parameters
+    ----------
+    path_database : str
+        Path to the folder where all the raw GST logger data are found
+    path_in : str
+        Path to the folder where the raw simulation data is found
+    path_out : str
+        Path to the processed output pickle
+    """
+    list_subsites_db = []
+    list_sites_db = []
+
+    for f in os.listdir(path_database):
+        if 'hourly' in f:
+            file = f.split('_hourly')[0]
+            list_subsites_db.append(file.split('-')[1])
+            list_sites_db.append(file.split('-')[1].split('_ST')[0])
+
+    list_subsites_db = sorted(list_subsites_db)
+    list_sites_db = list(np.unique(list_sites_db))
+
+    list_reanalysis = sorted(next(os.walk(path_in))[1])
+
+    metadata_sims_csv = {ra: [] for ra in list_reanalysis}
+
+    for ra in list_reanalysis:
+        metadata_sims_csv[ra] = pd.read_csv(os.path.join(path_in,f'{ra}/metadata.csv'))
+        metadata_sims_csv[ra] = metadata_sims_csv[ra].drop(columns=['id', 'model', 'parameters', 'site_name', 'forcing_name'])
+        metadata_sims_csv[ra]['site'] = [i.split('YGS_')[1] for i in metadata_sims_csv[ra]['site']]
+        metadata_sims_csv[ra]['forcing'] = [i.split('_stations')[0] for i in metadata_sims_csv[ra]['forcing']]
+        metadata_sims_csv[ra]['directory'] = [i.split('_')[-1] for i in metadata_sims_csv[ra]['directory']]
+        for var in ['topo', 'loc', 'forest', 'soil']:
+            metadata_sims_csv[ra][var] = [i.split(f'{var}_')[1] for i in metadata_sims_csv[ra][var]]
+        metadata_sims_csv[ra]['suffix'] = [metadata_sims_csv[ra]['topo'][i].split(loc)[1] for i,loc in enumerate(metadata_sims_csv[ra]['loc'])]
+        metadata_sims_csv[ra]['sub_site'] = [{'_a': 'ST01', '_b': 'ST02', '_c': 'ST03', '': ''}[i] for i in metadata_sims_csv[ra]['suffix']]
+        metadata_sims_csv[ra]['site_and_sub'] = [i+('' if j=='' else '_') +j for i,j in zip(metadata_sims_csv[ra]['site'], metadata_sims_csv[ra]['sub_site'])]
+        
+    list_sites_including_sub = sorted(np.unique(metadata_sims_csv[list_reanalysis[0]]['site_and_sub']))
+
+    # Here we compute the 'calibrated mean' as
+    # The mean of all simulations with 'mean SnowCorrFactor',
+    # 'mean SnowViscosity'
+
+    nc_soil = {ra: Dataset(os.path.join(path_in,f'{ra}/result_soil_temperature.nc'))['geotop'] for ra in list_reanalysis}
+
+    time_soil = pd.to_datetime([str(i) for i in num2date(nc_soil[list_reanalysis[0]]['Date'][:], nc_soil[list_reanalysis[0]]['Date'].units)])
+
+    df_Tg = {ra: pd.DataFrame(nc_soil[ra]['Tg'][:,:,0].T,
+                            index=time_soil,
+                            columns=[i.split('_')[-1] for i in nc_soil[ra]['simulation'][:]])
+            for ra in list_reanalysis}
+
+    df_Tg_sites_including_subs = {site: {ra: [] for ra in list_reanalysis} for site in list_sites_including_sub}
+    df_Tg_sites = {site: {ra: [] for ra in list_reanalysis} for site in list_sites_db}
+
+    for site in list_sites_db:
+        for ra in list_reanalysis:
+            df_meta = metadata_sims_csv[ra].loc[(metadata_sims_csv[ra]['site']==site)]
+            list_subs = sorted(np.unique(df_meta['sub_site']))
+            for sub in list_subs:
+                site_with_sub = site + ('' if sub=='' else f'_{sub}')
+                df_meta_sub = df_meta.loc[(df_meta['sub_site'])==sub]
+
+                list_scf = sorted(np.unique(df_meta_sub['SnowCorrFactor']))
+                list_visc = sorted(np.unique(df_meta_sub['SnowViscosity']))
+                all_dirs = list(df_meta_sub.directory)
+                calibrated_dir = list(df_meta_sub.loc[(df_meta_sub['SnowCorrFactor']==list_scf[1]) & (df_meta_sub['SnowViscosity']==list_visc[1]), 'directory'])
+                df_Tg_sites_including_subs[site_with_sub][ra] = df_Tg[ra][all_dirs].copy()
+                df_Tg_sites_including_subs[site_with_sub][ra]['GST_sim_min_C'] = df_Tg_sites_including_subs[site_with_sub][ra].min(axis=1)
+                df_Tg_sites_including_subs[site_with_sub][ra]['GST_sim_max_C'] = df_Tg_sites_including_subs[site_with_sub][ra].max(axis=1)
+                df_Tg_sites_including_subs[site_with_sub][ra]['GST_sim_calibrated_C'] = df_Tg_sites_including_subs[site_with_sub][ra][calibrated_dir].mean(axis=1)
+
+    for site in list_sites_db:
+        for ra in list_reanalysis:
+            if site in list_sites_including_sub:
+                df_Tg_sites[site][ra] = df_Tg_sites_including_subs[site][ra].copy()
+            else:
+                df_Tg_sites[site][ra] = pd.concat([df_Tg_sites_including_subs[f'{site}_ST0{i}'][ra].drop(columns=['GST_sim_min_C', 'GST_sim_max_C', 'GST_sim_calibrated_C']) for i in range(1,4)],
+                                                join='inner', axis=1)
+                df_Tg_sites[site][ra]['GST_sim_min_C'] = pd.concat([df_Tg_sites_including_subs[f'{site}_ST0{i}'][ra][['GST_sim_min_C']] for i in range(1,4)],
+                                                        join='inner', axis=1).min(axis=1)
+                df_Tg_sites[site][ra]['GST_sim_max_C'] = pd.concat([df_Tg_sites_including_subs[f'{site}_ST0{i}'][ra][['GST_sim_max_C']] for i in range(1,4)],
+                                                        join='inner', axis=1).max(axis=1)
+                df_Tg_sites[site][ra]['GST_sim_calibrated_C'] = pd.concat([df_Tg_sites_including_subs[f'{site}_ST0{i}'][ra][['GST_sim_calibrated_C']] for i in range(1,4)],
+                                                        join='inner', axis=1).mean(axis=1)
+
+    df_Tg_sites_summary = {site: {ra: [] for ra in list_reanalysis} for site in list_sites_db}
+    for site in list_sites_db:
+        for ra in list_reanalysis:
+            df_Tg_sites_summary[site][ra] = df_Tg_sites[site][ra][['GST_sim_min_C', 'GST_sim_max_C', 'GST_sim_calibrated_C']]
+
+    # Open a file and use dump() 
+    with open(os.path.join(path_out, 'df_Tg_sites_summary.pkl'), 'wb') as file: 
+        # A new file will be created 
+        pickle.dump(df_Tg_sites_summary, file)
+
+def format_paleo_Benthic(path_in, path_out):
+    """ Function formats Benthic paleo data into a csv
+    
+    Parameters
+    ----------
+    path_in : str
+        Path to the raw input .csv file where the Benthic paleo data is stored
+    path_out : str
+        Path to the processed output .csv file where the Benthic paleo data is stored  
+    """
+    df = pd.read_csv(path_in, comment='#', sep='\t')
+    df = df.rename(columns={k:v for k,v in zip(df.columns, ['age_ka', 'Benthic_d18O', 'se'])})
+    df = df.set_index('age_ka')[['Benthic_d18O']]
+    df.to_csv(path_out)
+
+def format_paleo_Greenland(path_in, path_out):
+    """ Function formats Greenland paleo data into a csv
+    
+    Parameters
+    ----------
+    path_in : str
+        Path to the raw input .csv file where the Greenland paleo data is stored
+    path_out : str
+        Path to the processed output .csv file where the Greenland paleo data is stored  
+    """
+    df = pd.read_csv(path_in, comment='#', sep='\t')
+    df['age_ka'] = pd.to_numeric(df["age_calBP1950"]) / 1000.0
+    df = df[['age_ka','d18O_smow']].rename(columns={'d18O_smow': 'Greenland_d18O'})
+    df = df.set_index('age_ka')
+    window=5
+    df['forward'] = df['Greenland_d18O'][::-1].rolling(window=window).mean()[::-1]
+    df['backward'] = df['Greenland_d18O'].rolling(window=window).mean()
+    df['Greenland_d18O'] = df[['forward','backward']].mean(skipna=True,axis=1)
+    df = df.loc[df.index<holocene_end_kaBP,['Greenland_d18O']]
+    df.to_csv(path_out)
+
+def format_paleo_Yukon(path_in, path_out):
+    """ Function formats Yukon paleo data into a csv
+    
+    Parameters
+    ----------
+    path_in : str
+        Path to the raw input .csv file where the Yukon paleo data is stored
+    path_out : str
+        Path to the processed output .csv file where the Yukon paleo data is stored  
+    """
+    df = pd.read_csv(path_in)
+    df['age_ka'] = df['Year_before_1950']/1000
+    df = df.set_index('age_ka')[['dT', 'dT_minus_1s', 'dT_plus_1s']]
+    df = df.loc[df.index<holocene_end_kaBP]
+    df.to_csv(path_out)
+
+def format_all_raw_data(path_wildfire_in, path_wildfire_out, path_gst_logger_in, path_gst_logger_out,
+                        path_forcing_ygs_sites_in, path_forcing_ygs_sites_out, path_simulations_ygs_in, path_simulations_ygs_out,
+                        path_paleo_Benthic_in, path_paleo_Benthic_out, path_paleo_Greenland_in, path_paleo_Greenland_out, path_paleo_Yukon_in, path_paleo_Yukon_out):
     """ Function formats all raw input data
     
     Parameters
@@ -133,6 +287,22 @@ def format_all_raw_data(path_wildfire_in, path_wildfire_out, path_gst_logger_in,
         Path to the folder where the raw forcing data is found
     path_forcing_ygs_sites_out : str
         Path to the processed output pickle
+    path_simulations_ygs_in : str
+        Path to the folder where the raw simulation data is found
+    path_simulations_ygs_out : str
+        Path to the processed output pickle
+    path_paleo_Benthic_in : str
+        Path to the raw input .csv file where the Benthic paleo data is stored
+    path_paleo_Benthic_out : str
+        Path to the processed output .csv file where the Benthic paleo data is stored 
+    path_paleo_Greenland_in : str
+        Path to the raw input .csv file where the Greenland paleo data is stored
+    path_paleo_Greenland_out : str
+        Path to the processed output .csv file where the Greenland paleo data is stored  
+    path_paleo_Yukon_in : str
+        Path to the raw input .csv file where the Yukon paleo data is stored
+    path_paleo_Yukon_out : str
+        Path to the processed output .csv file where the Yukon paleo data is stored 
 
     Returns
     -------
@@ -141,3 +311,7 @@ def format_all_raw_data(path_wildfire_in, path_wildfire_out, path_gst_logger_in,
     format_wildfire_data(path_wildfire_in, path_wildfire_out)
     format_gst_logger(path_gst_logger_in, path_gst_logger_out)
     format_forcing_ygs(path_forcing_ygs_sites_in, path_forcing_ygs_sites_out)
+    format_simulations_ygs(path_gst_logger_in, path_simulations_ygs_in, path_simulations_ygs_out)
+    format_paleo_Benthic(path_paleo_Benthic_in, path_paleo_Benthic_out)
+    format_paleo_Greenland(path_paleo_Greenland_in, path_paleo_Greenland_out)
+    format_paleo_Yukon(path_paleo_Yukon_in, path_paleo_Yukon_out)
